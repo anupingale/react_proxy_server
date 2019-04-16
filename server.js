33339e4454e54188ceacc981e54ba1f2ca916771
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2019-04-16 16:24:57 +0530

    Added the basic react app with proxy server.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 8080 || process.env.port;
+const port = process.env.port || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
